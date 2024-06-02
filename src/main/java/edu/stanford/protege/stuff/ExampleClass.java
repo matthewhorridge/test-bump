d8431511e95fa89568b27b16cf956cb89164f616
Matthew Horridge <matthew.horridge@stanford.edu>
2024-06-02 09:59:45 -0700

    Initial commit

---
+++ src/main/java/edu/stanford/protege/stuff/ExampleClass.java
@@ -0,0 +1,13 @@
+package edu.stanford.protege.stuff;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2024-06-02
+ */
+public class ExampleClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
