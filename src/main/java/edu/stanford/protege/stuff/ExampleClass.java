0e3f570bcfa113ad31ac6f569efd2970d2fcaf16
Matthew Horridge <matthew.horridge@stanford.edu>
2024-06-02 10:14:56 -0700

    Altered message

--- src/main/java/edu/stanford/protege/stuff/ExampleClass.java
+++ src/main/java/edu/stanford/protege/stuff/ExampleClass.java
@@ -8,6 +8,6 @@ package edu.stanford.protege.stuff;
 public class ExampleClass {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
     }
 }
